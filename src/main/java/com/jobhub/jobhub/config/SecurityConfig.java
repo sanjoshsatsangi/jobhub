b6a87b5bea57c140fc342697b46644e58a550b1d
Sanjosh Satsangi <sanjoshsatsangi@gmail.com>
2026-09-18 02:10:28 +0530

    Fix CORS security configuration

--- src/main/java/com/jobhub/jobhub/config/SecurityConfig.java
+++ src/main/java/com/jobhub/jobhub/config/SecurityConfig.java
@@ -3,14 +3,10 @@ package com.jobhub.jobhub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -26,8 +22,6 @@ public class SecurityConfig {
 
         http
             .csrf(AbstractHttpConfigurer::disable)
-
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
             .authorizeHttpRequests(auth -> auth
 
@@ -97,44 +91,5 @@ public class SecurityConfig {
             );
 
         return http.build();
-    }
-
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOriginPatterns(
-            List.of(
-                "http://localhost:5173",
-                "https://jobhub-gilt.vercel.app"
-            )
-        );
-
-        configuration.setAllowedMethods(
-            List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-            )
-        );
-
-        configuration.setAllowedHeaders(
-            List.of("*")
-        );
-
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration(
-            "/**",
-            configuration
-        );
-
-        return source;
     }
 }
